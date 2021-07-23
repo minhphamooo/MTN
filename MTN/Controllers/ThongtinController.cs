f626b6e6624b454bd339f813743a1ebe84eb76c2
minhphamooo <86455444+minhphamooo@users.noreply.github.com>
2021-07-23 18:24:45 +0700

    hello

---
+++ MTN/Controllers/ThongtinController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MTN.Controllers
+{
+    public class ThongtinController : Controller
+    {
+        // GET: Thongtin
+        public ActionResult Thongtinweb()
+        {
+            return View();
+        }
+    }
+}
