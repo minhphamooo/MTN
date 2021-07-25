52bd1351b03b9a571733e05ddc54a5c11d1a7400
minhphamooo <86455444+minhphamooo@users.noreply.github.com>
2021-07-25 12:37:53 +0700

    up all

--- MTN/Controllers/DienThoaiController.cs
+++ MTN/Controllers/DienThoaiController.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using MTN.Models;
 namespace MTN.Controllers
-{
+{//Minh
     public class DienThoaiController : Controller
     {
         dbQLdienthoaiDataContext data = new dbQLdienthoaiDataContext();
